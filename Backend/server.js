77d1c86d387e32310548db1ba3b3eb20b66d7984
Manjunath063 <manjunathjangam198@gmail.com>
2025-07-14 10:51:13 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -208,5 +208,5 @@ app.delete('/api/offboarding', async (req, res) => {
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server running at http://3.85.61.23:${port}`);
+    console.log(`Server running at http://16.170.227.238:${port}`);
 });
